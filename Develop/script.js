34f3a998848ac60540851b90e166345fbc4c411f
Conor <conor.reed@gmail.com>
2023-09-21 14:05:58 -0500

    added comments to the code to explain the functions and while loop with the password generator

--- Develop/script.js
+++ Develop/script.js
@@ -24,13 +24,14 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-//
+// function for yes or no prompt so store user decisions about whether they want lowercase, uppercase, special, or numeric characters
 function promptYesNo(prompt){
   if (window.confirm(prompt)) {
     return true;
   }
   return false;
 }
+// function that prompts the user to enter a number between 8 and 128 and then confirms that the input is within the valid range
 function generatePassword(){
   const numberOfCharacters = prompt("Please the length of your password that is between 8 and 128 characters");
   if (isNaN(numberOfCharacters)){
@@ -38,11 +39,12 @@ function generatePassword(){
     return "";
   }
   const passwordLength = parseInt(numberOfCharacters,10);
-
+// confirming the password length
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a number between 8 and 128");
     return "";
   }
+  // prompting with YesNo function that will store user choice
   const includeLowerCase = promptYesNo("Do you want your password to include lowercase?");
   
   const includeUpperCase = promptYesNo("Do you want your password to include uppercase?");
@@ -50,19 +52,19 @@ function generatePassword(){
   const includeNumericCharacters =  promptYesNo("Do you want your password to include numeric?");
   
   const includeSpecialCharacters = promptYesNo("Do you want your password to include special?");
-
+// confirming that at least one character type is selected, otherwise no password would be generated
   if (!includeLowerCase && !includeUpperCase && !includeNumericCharacters && !includeSpecialCharacters){
     alert("You must select at least one character type")
     return "";
   }
-  
+  // variables that will hold each character type in a string
   const lowerCaseCharacters = "qwertyuiopasdfghjklzxcvbnm";
   const upperCaseCharacters = "QWERTYUIOPASDFGHJKLZXCVBNM";
   const numericCharacters = "1234567890";
   const specialCharacters = "!@#$%^&*+";
 
   let password = "";
-
+// while loop that will iterate through their case choices and then pull a random value from the string variables and concate it with the password string
   while (password.length < passwordLength){
     if (includeLowerCase){
       const lower = lowerCaseCharacters.charAt(getRandomIntInclusive(0,lowerCaseCharacters.length -1));
@@ -81,7 +83,7 @@ function generatePassword(){
       password += special;
     }
   }
-
+// using substring to make sure that the password doesn't accidently go over the 128 character limit if they picked all special characters and a max length password. This will make sure it doesn't go to like 131 characters or something
   password = password.substring(0,passwordLength);
   console.log(password.length);
   return password;
